798b501f15c94f6b5cf4e5d578ba285fad379e2b
Christian Pedersen <crpe44102@edu.ucl.dk>
2022-10-12 12:47:48 +0200

    implement the ability to create a new post (non-functional)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,10 @@ const postResponseCharacterCounter = document.getElementById("post-response-char
 postResponseTextarea.addEventListener("keyup", event => {
     postResponseCharacterCounter.textContent = event.currentTarget.value.length
 })
+
+const postCreationTextarea = document.getElementById("post-creation-textarea")
+const postCreationCharacterCounter = document.getElementById("post-creation-character-counter")
+
+postCreationTextarea.addEventListener("keyup", event => {
+    postCreationCharacterCounter.textContent = event.currentTarget.value.length
+})
